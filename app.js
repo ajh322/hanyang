57941762cdd29aa221d1df7f45ae627848770a51
ajh322 <jihwan322@gmail.com>
2016-11-12 04:31:43 +0900

    로그인 거의 다만듬

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var http = require('http');
 var server = http.createServer(app);
 var bodyParser = require('body-parser');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://35.163.104.205:27017/user');
 var conn = mongoose.connection;
 var user = require('./models/user');
@@ -19,7 +20,7 @@ app.listen(9000, function () {
 app.get('/', function (req, res) {
     console.log("get");
     var user = {
-        user_id: "ajh322",
+        user_id: 2016022288,
         user_pw: "1234",
         user_name: "지화닝",
         user_gender: "m",
@@ -55,6 +56,7 @@ app.post('/login', function (req, res) {
     console.log(req.body);
     user.find({user_id: req.body.user_id, user_pw:req.body.user_pw}).exec(function (err, doc) {
         //나중에 로그인 가능여부 판별후에 해야함.
+        console.log(doc);
         if (doc != null) //로그인 성공
         {
             //최근로그인기록이나 누적로그인 회수도 기록할까? 이런거 기록하자
