9629436604ed0928d3aee2c2e84943ec4d3e6bfe
ajh322 <jihwan322@gmail.com>
2016-11-12 17:13:33 +0900

    채팅 socket.io 생성중

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://35.163.104.205:27017/user');
 var conn = mongoose.connection;
 var user = require('./models/user');
-
+var socketio = require('socket.io');
+var io = socketio.listen(server);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -30,29 +31,49 @@ app.get('/', function (req, res) {
     conn.collection('user').insert(user);
     res.send('Hello World!');
 });
+
+io.sockets.on('connection', function (socket) {
+    //room join
+    socket.on('join', function (data) {
+        console.log(data.userid + "joined" + "roomname:" + data.roomname);
+        socket.join(data.roomname);
+        socket.set('room', data.roomname);
+        socket.get('room', function (error, room) {
+            io.sockets.in(room).emit('join', data.userid);
+        })
+    })
+    socket.on('message', function (message) {
+        console.log("message:" + data.message);
+        socket.get('room', function (error, room) {
+            io.sockets.in(room).emit('message', message);
+        })
+    })
+    socket.on('disconnection', function () {
+        console.log("disconnected");
+    });
+})
+
 app.post('/sign_in', function (req, res) {
     console.log('get');
     console.log(req.body);
     user.find({user_id: req.body.user_id}).exec(function (err, doc) {
-console.log(doc);    
-    //나중에 로그인 가능여부 판별후에 해야함.
-        if (doc == "")
-        {
-            if(true)//api 사용하여 존재하는 학번, 재학생인지 판단인지 확인
+        console.log(doc);
+        //나중에 로그인 가능여부 판별후에 해야함.
+        if (doc == "") {
+            if (true)//api 사용하여 존재하는 학번, 재학생인지 판단인지 확인
             {
-            var user = {
-                user_id: req.body.user_id,
-                user_pw: req.body.user_pw,
-                user_name: req.body.user_name,
-                //user_gender: req.body.user_gender, api에서 받아오기
-                //user_species: req.body.user_species, api에서 받아오기
-                user_first: "1"
-            };
-            conn.collection('user').insert(user);
-            res.end("success");
+                var user = {
+                    user_id: req.body.user_id,
+                    user_pw: req.body.user_pw,
+                    user_name: req.body.user_name,
+                    //user_gender: req.body.user_gender, api에서 받아오기
+                    //user_species: req.body.user_species, api에서 받아오기
+                    user_first: "1"
+                };
+                conn.collection('user').insert(user);
+                res.end("success");
             }
-            else
-            {
+            else {
                 res.end("ID not exist")
             }
         }
@@ -62,10 +83,10 @@ console.log(doc);
     })
 });
 app.post('/login', function (req, res) {
-    console.log(req.body.user_id+req.body.user_pw);
-    user.find({user_id: req.body.user_id, user_pw:req.body.user_pw}).lean().exec(function (err, doc) {
+    console.log(req.body);
+    user.find({user_id: req.body.user_id, user_pw: req.body.user_pw}).exec(function (err, doc) {
         //나중에 로그인 가능여부 판별후에 해야함.
-console.log(doc);
+        console.log(doc);
         if (doc != "") //로그인 성공
         {
             //최근로그인기록이나 누적로그인 회수도 기록할까? 이런거 기록하자
