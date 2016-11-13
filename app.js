63bccbd82f613fb3e7c21d2d9230824a1e351cab
ajh322 <jihwan322@gmail.com>
2016-11-14 05:19:15 +0900

    gcm 토큰 전송기능 추가

--- app.js
+++ app.js
@@ -60,7 +60,10 @@ app.get('/', function (req, res) {
      res.send('Hello World!');*/
 });
 
-
+app.post('/send_token', function (req, res) {
+    console.log('token');
+    console.log(req.body);
+})
 app.post('/sign_in', function (req, res) {
     console.log('get');
     console.log(req.body);
