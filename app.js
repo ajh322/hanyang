c6d7d70c93b605d7ff3c66d1e5f61925800cf817
AJH <jihwan322@gmail.com>
2016-12-11 04:55:51 +0900

    matching debugging

--- app.js
+++ app.js
@@ -225,14 +225,14 @@ app.post('/test_ans', function (req, res) {
                     sendMessageToUser(doc_l.user_token, {status: "chat"});
                     sendMessageToUser(doc.user_token, {status: "chat"});
                     console.log("1");
-                    res.end("fighting");
+                    res.end("chat");
                 }
                 else {
                     doc.user_like = "1";
                     doc.save();
                     sendMessageToUser(doc_l.user_token, {status: "one_more"}); //상대방에게 좋아요
                     console.log("2");
-                    res.end("fighting");
+                    res.end("yet");
                 }
             })
 
