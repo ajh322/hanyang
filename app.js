73dbf16236821fead3d9d60b2c3b6a9e7f1e295f
AJH <jihwan322@gmail.com>
2016-12-13 02:49:24 +0900

    add_chat sendMessage fix

--- app.js
+++ app.js
@@ -273,11 +273,20 @@ app.post('/add_chat', function (req, res) {
     console.log(req.body);
     try {
         user.findOne({user_id: req.body.user_id}).exec(function (err, doc) {
+
+            var val = 0; //msg index
+            var target_token=""; //target token
+
+            //target token initialize
+            user.findOne({user_id:doc.user_target_id}).exec(function (err, doc_1) {
+                target_token=doc_1.target_token;
+            })
+
             //find the index
             get_chat_model(doc.chat_name).findOne({}).sort('-index').exec(function (err, doc_l) {
-                var val = 0;
+
                 //send notification to target_id
-                sendMessageToUser(doc.user_target_id, {status: "add_chat", msg:req.body.msg});
+                sendMessageToUser(target_token, {status: "add_chat", msg:req.body.msg});
                 val = doc_l.index;
                 console.log("index num:" + val);
 
