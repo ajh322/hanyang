0e6bbf905cc7ff65ff4c942cb36d653dbb5bb01c
AJH <jihwan322@gmail.com>
2016-12-15 00:33:32 +0900

    add_chatdata

--- app.js
+++ app.js
@@ -341,8 +341,12 @@ app.post('/add_chat', function (req, res) {
             get_chat_model(doc.chat_name).findOne({}).sort('-index').exec(function (err, doc_l) {
 
                 //send notification to target_id
-                //if (target_token != doc.user_token)
+                if (target_token != doc.user_token) //이거때문에 디버깅 불가능함.
+                {
+                    console.log("target_token:"+target_token);
+                    console.log("doc.user_token:"+doc.user_token);
                     sendMessageToUser(target_token, {status: "add_chat", msg: req.body.msg});
+                }
                 val = doc_l.index;
                 console.log("index num:" + val);
 
